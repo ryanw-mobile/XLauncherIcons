a58af750301c5c4466c5b577295747ea1d5a3282
Ryan Wong <ryan@rwmobi.com>
2023-08-01 23:38:45 +0100

    [chore] App: update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,7 @@ android {
         buildConfig = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
+        kotlinCompilerExtensionVersion = "1.5.1"
     }
     packaging {
         resources {
