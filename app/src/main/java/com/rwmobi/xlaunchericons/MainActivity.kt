b2b4d1c37f8e86cff6d770180132be420fedbe62
Ryan Wong <ryan@rwmobi.com>
2023-08-01 23:55:52 +0100

    [feature] UI: Subscribe to greg for only $1

--- app/src/main/java/com/rwmobi/xlaunchericons/MainActivity.kt
+++ app/src/main/java/com/rwmobi/xlaunchericons/MainActivity.kt
@@ -6,19 +6,28 @@ import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.rwmobi.xlaunchericons.ui.theme.XLauncherIconsTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,7 +40,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting(
+                    MainScreen(
                         modifier = Modifier.fillMaxWidth()
                     )
                 }
@@ -41,56 +50,77 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(modifier: Modifier = Modifier) {
+fun MainScreen(modifier: Modifier = Modifier) {
     val context = LocalContext.current
 
-    Row(
-        modifier = modifier
-    ) {
-        Icon(
-            modifier = Modifier.clickable(enabled = true, onClick = {
-                setIcon(
-                    context = context,
-                    componentName = "com.rwmobi.xlaunchericons.MainActivityA"
-                )
-            }),
-            painter = painterResource(id = R.drawable.ic_launcher_a_foreground),
-            contentDescription = null
+    Column(modifier = modifier.background(color = Color(0xFF161D26))) {
+
+        Spacer(modifier = Modifier.size(48.dp))
+
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            text = "Subscribe to greg for only $1",
+            color = Color.LightGray,
+            style = MaterialTheme.typography.headlineMedium,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center
         )
 
-        Spacer(modifier = Modifier.weight(1.0f))
+        Spacer(modifier = Modifier.size(48.dp))
 
-        Icon(
-            modifier = Modifier.clickable(enabled = true, onClick = {
-                setIcon(
-                    context = context,
-                    componentName = "com.rwmobi.xlaunchericons.MainActivityB"
-                )
-            }),
-            painter = painterResource(id = R.drawable.ic_launcher_b_foreground),
-            contentDescription = null
-        )
+        Row(
 
-        Spacer(modifier = Modifier.weight(1.0f))
+        ) {
 
-        Icon(
-            modifier = Modifier.clickable(enabled = true, onClick = {
-                setIcon(
-                    context = context,
-                    componentName = "com.rwmobi.xlaunchericons.MainActivityC"
-                )
-            }),
-            painter = painterResource(id = R.drawable.ic_launcher_c_foreground),
-            contentDescription = null
-        )
+
+            Icon(
+                modifier = Modifier.clickable(enabled = true, onClick = {
+                    setIcon(
+                        context = context,
+                        componentName = "com.rwmobi.xlaunchericons.MainActivityA"
+                    )
+                }),
+                painter = painterResource(id = R.drawable.ic_launcher_a_foreground),
+                contentDescription = null
+            )
+
+            Spacer(modifier = Modifier.weight(1.0f))
+
+            Icon(
+                modifier = Modifier.clickable(enabled = true, onClick = {
+                    setIcon(
+                        context = context,
+                        componentName = "com.rwmobi.xlaunchericons.MainActivityB"
+                    )
+                }),
+                painter = painterResource(id = R.drawable.ic_launcher_b_foreground),
+                contentDescription = null
+            )
+
+            Spacer(modifier = Modifier.weight(1.0f))
+
+            Icon(
+                modifier = Modifier.clickable(enabled = true, onClick = {
+                    setIcon(
+                        context = context,
+                        componentName = "com.rwmobi.xlaunchericons.MainActivityC"
+                    )
+                }),
+                painter = painterResource(id = R.drawable.ic_launcher_c_foreground),
+                contentDescription = null
+            )
+        }
     }
 }
 
-@Preview(showBackground = true)
+@Preview(
+    showBackground = true,
+    showSystemUi = true,
+)
 @Composable
 fun GreetingPreview() {
     XLauncherIconsTheme {
-        Greeting(modifier = Modifier.fillMaxWidth())
+        MainScreen(modifier = Modifier.fillMaxWidth())
     }
 }
 
