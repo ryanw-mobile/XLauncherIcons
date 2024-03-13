4de2dd9ffde51e717c88f3d286ebc5211292f03a
Ryan Wong <ryan@rwmobi.com>
2024-03-13 17:17:59 +0000

    [chore] CI: introduce release build yaml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,9 @@
+import java.io.FileInputStream
+import java.io.InputStreamReader
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Properties
+
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
     alias(libs.plugins.androidApplication)
@@ -9,28 +15,95 @@ android {
     namespace = "com.rwmobi.xlaunchericons"
     compileSdk = 34
 
+    signingConfigs {
+        create("release") {
+            val isRunningOnCI = System.getenv("BITRISE") == "true"
+            val keystorePropertiesFile = file("../../keystore.properties")
+
+            if (isRunningOnCI) {
+                println("Signing Config: using environment variables")
+                keyAlias = System.getenv("BITRISEIO_ANDROID_KEYSTORE_ALIAS")
+                keyPassword = System.getenv("BITRISEIO_ANDROID_KEYSTORE_PRIVATE_KEY_PASSWORD")
+                storeFile = file(System.getenv("KEYSTORE_LOCATION"))
+                storePassword = System.getenv("BITRISEIO_ANDROID_KEYSTORE_PASSWORD")
+            } else if (keystorePropertiesFile.exists()) {
+                println("Signing Config: using keystore properties")
+                val properties = Properties()
+                InputStreamReader(
+                    FileInputStream(keystorePropertiesFile),
+                    Charsets.UTF_8,
+                ).use { reader ->
+                    properties.load(reader)
+                }
+
+                keyAlias = properties.getProperty("alias")
+                keyPassword = properties.getProperty("pass")
+                storeFile = file(properties.getProperty("store"))
+                storePassword = properties.getProperty("storePass")
+            } else {
+                println("Signing Config: skipping signing")
+            }
+        }
+    }
+
     defaultConfig {
         applicationId = "com.rwmobi.xlaunchericons"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
         }
+
+        // Bundle output filename
+        val timestamp = SimpleDateFormat("yyyyMMdd-HHmmss").format(Date())
+        setProperty("archivesBaseName", "XLauncherIcons-$versionName-$timestamp")
     }
 
     buildTypes {
-        release {
+        fun setOutputFileName() {
+            applicationVariants.all {
+                val variant = this
+                variant.outputs
+                    .map { it as com.android.build.gradle.internal.api.BaseVariantOutputImpl }
+                    .forEach { output ->
+                        val timestamp = SimpleDateFormat("yyyyMMdd-HHmmss").format(Date())
+                        val outputFileName =
+                            "XLauncherIcons-${variant.versionName}-$timestamp-${variant.name}.apk"
+                        output.outputFileName = outputFileName
+                    }
+            }
+        }
+
+        getByName("debug") {
+            applicationIdSuffix = ".debug"
             isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro",
+            isDebuggable = true
+            setOutputFileName()
+        }
+
+        getByName("release") {
+            isShrinkResources = true
+            isMinifyEnabled = true
+            isDebuggable = false
+            setProguardFiles(
+                listOf(
+                    getDefaultProguardFile("proguard-android-optimize.txt"),
+                    "proguard-rules.pro",
+                ),
             )
+
+            signingConfigs.getByName("release").keyAlias?.let {
+                signingConfig = signingConfigs.getByName("release")
+            }
+
+            setOutputFileName()
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
